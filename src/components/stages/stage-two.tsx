6e58315e3265751b57db195724ddaf1b8fba43e7
Zerosiini <eero.eklund@aalto.fi>
2023-07-12 14:07:46 +0300

    Current time implemented

--- src/components/stages/stage-two.tsx
+++ src/components/stages/stage-two.tsx
@@ -3,10 +3,18 @@ import { ValidationSchema } from "@/components/form";
 import type { FieldErrors, UseFormRegister } from "react-hook-form";
 import { FormRow } from "@/components/form-row";
 
-export const stageTwoSchema = z.object({
-  testi: z.string().min(1, { message: "Testi is required." }),
-});
-
+export const stageTwoSchema = z
+  .object({
+    currentTime: z.string().min(1, { message: "Time is required." }),
+    testi: z.string().min(1, { message: "Testi is required." }),
+  })
+  .refine(
+    (data) => Math.abs(Date.parse(data.currentTime) - Date.now()) < 60 * 1000, // times differ by less than a minute
+    {
+      path: ["currentTime"],
+      message: "Time must be the current time.",
+    },
+  );
 export function StageTwo({
   register,
   errors,
@@ -16,6 +24,17 @@ export function StageTwo({
 }) {
   return (
     <>
+      <FormRow
+        id="time"
+        label="Current time"
+        errors={errors.currentTime}
+        inputProps={{
+          type: "datetime-local",
+          id: "time",
+          placeholder: "Current time",
+          ...register("currentTime", { required: true }),
+        }}
+      />
       <FormRow
         id="testi"
         label="Testi"
